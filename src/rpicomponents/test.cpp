1342687a43c0d1794744de28e75dd03266a3a5a6
Krenol <lauritzgroeger@gmail.com>
2019-05-03 16:41:09 +0200

    changes

---
+++ src/rpicomponents/test.cpp
@@ -0,0 +1,12 @@
+
+#include "rpicomponents.hpp"
+
+using namespace std;
+using namespace rpicomponents;
+using namespace rpicomponents::component;
+using namespace rpicomponents::pin;
+
+int main() {
+	cout << "bla\n";
+	cin.get();
+}
