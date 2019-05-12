f5c86ef51dc84129a9a198bd53adf200497ab5d6
Krenol <lauritzgroeger@gmail.com>
2019-05-12 21:25:54 +0200

    new project structure

--- rpicomponents/test/src/test.cpp
+++ rpicomponents/test/src/test.cpp
@@ -1,14 +1,36 @@
 #include "../../include/rpicomponents/rpicomponents.hpp"
+#include <thread>
 
 using namespace std;
 using namespace rpicomponents;
 using namespace rpicomponents::component;
 using namespace rpicomponents::pin;
 
+static mutex mtx;
+
+void On(Led* led, int no){
+    //lock_guard<mutex> lck(mtx);
+
+    for(auto i= 0; i <  no; i++) {
+        auto on = led->IsOn();
+        cout << "thread " << no << " led is " << on<<endl;
+        on ? led->TurnOff() : led->TurnOn();
+    }
+
+    return;
+}
+
+
 int main() {
 	cout << "bla\n";
-    auto btn = new Button(GPIO2, PUD_UP);
-    cout << btn->IsPressed()<<endl;
-	cin.get();
-    delete btn;
+    auto led = new Led(GPIO2, DIGITAL);
+    //cout << btn->IsPressed()<<endl;
+
+    for(int i = 0; i < 10; i++) {
+        thread p(On, led, i);
+        thread p1(On, led, i);
+        p.join();
+        p1.join();
+    }
+    delete led;
 }
