fdaa55bf56b62869ec3900a2082343771107afa5
Krenol <lauritzgroeger@gmail.com>
2019-06-13 20:53:34 -0400

    tried adding i2cdevlib as seperate so; by now not working

---
+++ rpicomponents/external/i2c/include/i2cdevlib/i2cdevlib.hpp
@@ -0,0 +1,8 @@
+#ifndef EXTERNAL_I2CDEVLIB_H
+#define EXTERNAL_I2CDEVLIB_H
+#pragma once
+
+#include "../../i2cdevlib/Arduino/MPU6050/MPU6050.h"
+#include "../../i2cdevlib/RaspberryPi_bcm2835/I2Cdev/I2Cdev.h
+
+#endif
