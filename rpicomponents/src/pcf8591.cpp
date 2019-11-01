04d4bdf84e212fd2af1a80dfc7bb84b8e8d6038a
Krenol <lauritzgroeger@gmail.com>
2019-11-01 14:58:45 +0100

    updated pcf8591

--- rpicomponents/src/pcf8591.cpp
+++ rpicomponents/src/pcf8591.cpp
@@ -2,15 +2,24 @@
 #include <pcf8591.h>
 
 
-rpicomponents::Pcf8591::Pcf8591(int address, int pin_base) : Pcf(address, pin_base, COMPONENT_PCF8591_READ_PINS, COMPONENT_PCF8591_WRITE_PINS, 
+void rpicomponents::Pcf8591::WriteToPcfPin(const int& pcf_pin_no, const int& value) const {
+	WriteToPcfPin(value);
+}
+
+rpicomponents::Pcf8591::Pcf8591(const int& address, const int& pin_base) : Pcf(address, pin_base, COMPONENT_PCF8591_READ_PINS, COMPONENT_PCF8591_WRITE_PINS,
 	COMPONENT_PCF8591_MIN_PIN_OFFSET, COMPONENT_PCF8591_MAX_PIN_OFFSET, COMPONENT_PCF8591)
 {
 	pcf8591Setup(pin_base_, address_);
 }
 
-void rpicomponents::Pcf8591::WriteToPcfPin(int pcf_pin_no, const int& value) const {
-	if (pcf_pin_no != 0) return;
-	WriteToPcfPin(value);
+rpicomponents::Pcf8591::Pcf8591(int&& address, int&& pin_base) : Pcf(address, pin_base, COMPONENT_PCF8591_READ_PINS, COMPONENT_PCF8591_WRITE_PINS,
+	COMPONENT_PCF8591_MIN_PIN_OFFSET, COMPONENT_PCF8591_MAX_PIN_OFFSET, COMPONENT_PCF8591)
+{
+	pcf8591Setup(pin_base_, address_);
+}
+
+rpicomponents::Pcf8591::Pcf8591(const Pcf8591& pcf) : Pcf8591(pcf.GetPcfAddress(), pcf.GetPinBase())
+{
 }
 
 void rpicomponents::Pcf8591::WriteToPcfPin(const int& value) const {
@@ -19,7 +28,7 @@ void rpicomponents::Pcf8591::WriteToPcfPin(const int& value) const {
 }
 
 
-int rpicomponents::Pcf8591::ReadFromPcfPin(int pcf_pin_no) const {
+int rpicomponents::Pcf8591::ReadFromPcfPin(const int& pcf_pin_no) const {
 	if (!CheckIfPcfPin(pcf_pin_no)) return -1;
 	return analogRead(pin_base_ + pcf_pin_no);
 }
--- rpicomponents/src/pcf8591.hpp
+++ rpicomponents/src/pcf8591.hpp
@@ -16,7 +16,23 @@ namespace rpicomponents {
 		@param address: The i2c address of the pcf8591 (must be greater than 63)
 		@param pin_base: The pin base of the i2c component
 		*/
-		Pcf8591(int address, int pin_base = 64);
+		Pcf8591(const int& address, const int& pin_base = 64);
+
+		/*
+		Constructor
+
+		@param address: The i2c address of the pcf8591 (must be greater than 63)
+		@param pin_base: The pin base of the i2c component
+		*/
+		Pcf8591(int&& address, int&& pin_base = 64);
+
+		/*
+		Constructor
+
+		@param address: The i2c address of the pcf8591 (must be greater than 63)
+		@param pin_base: The pin base of the i2c component
+		*/
+		Pcf8591(const Pcf8591& pcf);
 
 		/*
 		Method that writes output to a pcf pin
@@ -31,7 +47,7 @@ namespace rpicomponents {
 		@param pcf_pin_no: The pcf pin to be written to
 		@param value: The value to be written (0-255)
 		*/
-		void WriteToPcfPin(int pcf_pin_no, const int& value) const;
+		void WriteToPcfPin(const int& pcf_pin_no, const int& value) const;
 
 		/*
 		Method that reads the input to a pcf pin
@@ -39,7 +55,7 @@ namespace rpicomponents {
 		@param pcf_pin_no: The pcf pin to be read
 		@returns: the read pin value (0 to 255)
 		*/
-		int ReadFromPcfPin(int pcf_pin_no) const;
+		int ReadFromPcfPin(const int& pcf_pin_no) const;
 	};
 }
 #endif
