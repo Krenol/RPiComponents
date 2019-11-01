6b56316869f662e4edbb53a562314226d6f7b55d
Krenol <lauritzgroeger@gmail.com>
2019-11-01 14:27:57 +0100

    updated joystick

--- rpicomponents/src/joystick.cpp
+++ rpicomponents/src/joystick.cpp
@@ -2,14 +2,21 @@
 
 void rpicomponents::Joystick::Initialize() const
 {
+	if (pcfXPin_ < 0) throw new std::invalid_argument("joystick pcf x pin cannot be lower than 0");
+	if (pcfYPin_ < 0) throw new std::invalid_argument("joystick pcf y pin cannot be lower than 0");
+
+	if (pcfXPin_ > pcf_->GetMaxPinOffset()) throw new std::invalid_argument("joystick pcf x pin must be < " + std::to_string(pcf_->GetMaxPinOffset()));
+	if (pcfYPin_ > pcf_->GetMaxPinOffset()) throw new std::invalid_argument("joystick pcf y pin must be < " + std::to_string(pcf_->GetMaxPinOffset()));
 }
 
-rpicomponents::Joystick::Joystick(const int& pcf_address, const int& pin_base, const int& pcf_x_pin, const int& pcf_y_pin, const int& gpio_z, const int& z_pud) : Component(COMPONENT_JOYSTICK)
+rpicomponents::Joystick::Joystick(const int& pcf_address, const int& pin_base, const int& pcf_x_pin, const int& pcf_y_pin, const int& gpio_z, const int& z_pud) : Component(COMPONENT_JOYSTICK), 
+	zBtn_{std::unique_ptr<Button>(new Button(gpio_z, z_pud))}, pcf_{std::unique_ptr<Pcf8591>(new Pcf8591(pcf_address, pin_base))}, pcfXPin_{ pcf_x_pin }, pcfYPin_{pcf_y_pin}
 {
 	Initialize();
 }
 
-rpicomponents::Joystick::Joystick(int&& pcf_address, int&& pin_base, int&& pcf_x_pin, int&& pcf_y_pin, int&& gpio_z, int&& z_pud) : Component(COMPONENT_JOYSTICK)
+rpicomponents::Joystick::Joystick(int&& pcf_address, int&& pin_base, int&& pcf_x_pin, int&& pcf_y_pin, int&& gpio_z, int&& z_pud) : Component(COMPONENT_JOYSTICK),
+	zBtn_{ std::unique_ptr<Button>(new Button(gpio_z, z_pud)) }, pcf_{ std::unique_ptr<Pcf8591>(new Pcf8591(pcf_address, pin_base)) }, pcfXPin_{ pcf_x_pin }, pcfYPin_{ pcf_y_pin }
 {
 	Initialize();
 }
@@ -21,50 +28,50 @@ rpicomponents::Joystick::Joystick(const Joystick& joystick) : Joystick(joystick.
 
 int rpicomponents::Joystick::ReadXAxis() const
 {
-	return 0;
+	return pcf_->ReadFromPcfPin(pcfXPin_);
 }
 
 int rpicomponents::Joystick::ReadYAxis() const
 {
-	return 0;
+	return pcf_->ReadFromPcfPin(pcfYPin_);
 }
 
 bool rpicomponents::Joystick::ReadZAxis() const
 {
-	return false;
+	return zBtn_->IsPressed();
 }
 
-JoystickAxes rpicomponents::Joystick::ReadAxes() const
+rpicomponents::JoystickAxes rpicomponents::Joystick::ReadAxes() const
 {
-	return JoystickAxes();
+	return JoystickAxes(ReadXAxis(), ReadYAxis(), ReadZAxis());
 }
 
 const int& rpicomponents::Joystick::GetPcfBase() const
 {
-	// TODO: insert return statement here
+	return pcf_->GetPinBase();
 }
 
 const int& rpicomponents::Joystick::GetPcfAddress() const
 {
-	// TODO: insert return statement here
+	return pcf_->GetPcfAddress();
 }
 
 const int& rpicomponents::Joystick::GetPcfXPin() const
 {
-	// TODO: insert return statement here
+	return pcfXPin_;
 }
 
 const int& rpicomponents::Joystick::GetPcfYPin() const
 {
-	// TODO: insert return statement here
+	return pcfYPin_;
 }
 
 const int& rpicomponents::Joystick::GetGpioZPin() const
 {
-	// TODO: insert return statement here
+	return zBtn_->GetPin();
 }
 
 const int& rpicomponents::Joystick::GetZPud() const
 {
-	// TODO: insert return statement here
+	return zBtn_->GetPUD();
 }
