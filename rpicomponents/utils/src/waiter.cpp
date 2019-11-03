d4f02a10e7ad82b26d5d1ec5e31e78b3c32cac6c
Krenol <lauritzgroeger@gmail.com>
2019-11-03 16:43:30 +0100

    Updated waiter with const refreneces as input

--- rpicomponents/utils/src/waiter.cpp
+++ rpicomponents/utils/src/waiter.cpp
@@ -7,37 +7,37 @@ Waiter::Waiter() {}
 
 Waiter::~Waiter() {}
 
-void Waiter::SleepNanos(std::chrono::nanoseconds nanos) 
+void Waiter::SleepNanos(const std::chrono::nanoseconds& nanos)
 {
 	std::thread p([&, nanos] {std::this_thread::sleep_for(nanos); });
 	p.join();
 }
 
-void Waiter::SleepNanos(long nanos) 
+void Waiter::SleepNanos(const long& nanos)
 {
 	std::thread p([&, nanos] {std::this_thread::sleep_for(std::chrono::nanoseconds(nanos)); });
 	p.join();
 }
 
-void Waiter::SleepMillis(std::chrono::milliseconds millis) 
+void Waiter::SleepMillis(const std::chrono::milliseconds& millis)
 {
 	std::thread p([&, millis] {std::this_thread::sleep_for(millis); });
 	p.join();
 }
 
-void Waiter::SleepMillis(long millis) 
+void Waiter::SleepMillis(const long& millis)
 {
 	std::thread p([&, millis] {std::this_thread::sleep_for(std::chrono::nanoseconds(millis)); });
 	p.join();
 }
 
-void Waiter::SleepSecs(std::chrono::seconds secs) 
+void Waiter::SleepSecs(const std::chrono::seconds& secs)
 {
 	std::thread p([&, secs] {std::this_thread::sleep_for(secs); });
 	p.join();
 }
 
-void Waiter::SleepSecs(long secs) 
+void Waiter::SleepSecs(const long& secs)
 {
 	std::thread p([&, secs] {std::this_thread::sleep_for(std::chrono::nanoseconds(secs)); });
 	p.join();
--- rpicomponents/utils/src/waiter.hpp
+++ rpicomponents/utils/src/waiter.hpp
@@ -14,37 +14,37 @@ namespace rpicomponents {
 			* Method that lets a new thread sleep for a certain amount of nanoseconds
 			* @param nanos: the amount of nanoseconds for the thread sleep
 			*/
-			static void SleepNanos(std::chrono::nanoseconds nanos);
+			static void SleepNanos(const std::chrono::nanoseconds& nanos);
 
 			/**
 			* Method that lets a new thread sleep for a certain amount of nanoseconds
 			* @param nanos: the amount of nanoseconds for the thread sleep
 			*/
-			static void SleepNanos(long nanos);
+			static void SleepNanos(const long& nanos);
 
 			/**
 			* Method that lets a new thread sleep for a certain amount of milliseconds
 			* @param millis: the amount of milliseconds for the thread sleep
 			*/
-			static void SleepMillis(std::chrono::milliseconds millis);
+			static void SleepMillis(const std::chrono::milliseconds& millis);
 
 			/**
 			* Method that lets a new thread sleep for a certain amount of milliseconds
 			* @param millis: the amount of milliseconds for the thread sleep
 			*/
-			static void SleepMillis(long millis);
+			static void SleepMillis(const long& millis);
 
 			/**
 			* Method that lets a new thread sleep for a certain amount of seconds
 			* @param secs: the amount of seconds for the thread sleep
 			*/
-			static void SleepSecs(std::chrono::seconds secs);
+			static void SleepSecs(const std::chrono::seconds& secs);
 
 			/**
 			* Method that lets a new thread sleep for a certain amount of seconds
 			* @param secs: the amount of seconds for the thread sleep
 			*/
-			static void SleepSecs(long secs);
+			static void SleepSecs(const long& secs);
 
 		private:
 			/**
