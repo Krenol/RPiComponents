222f0efb541697e504056efe839a824cb2aa3b75
Krenol <lauritzgroeger@gmail.com>
2020-01-07 19:12:32 +0100

    code update with for loop with iterators

--- rpicomponents/pin/src/addresschecker.cpp
+++ rpicomponents/pin/src/addresschecker.cpp
@@ -7,8 +7,28 @@ bool rpicomponents::pin::AddressChecker::IsI2CAddress(int address) {
 	const std::vector<char> i2c_col = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 	const std::vector<char> i2c_row = { '0', '1', '2', '3', '4', '5', '6', '7' };
 	auto hexAddr = GetHexAddress(address);
+	std::string addr;
+	auto rowLgth = std::distance(i2c_row.begin(), i2c_row.end());
+	int rowPos, colPos;
 
-	for (int i = 0; i < i2c_row.size(); i++)
+	for(auto row = i2c_row.begin(); row != i2c_row.end(); ++row) {
+		for(auto col = i2c_col.begin(); col != i2c_col.end(); ++col) {
+			addr.clear();
+			rowPos = std::distance(i2c_row.begin(), row);
+			colPos = std::distance(i2c_col.begin(), col);
+
+			if (rowPos == 0 && colPos < 3) continue;
+
+			if (rowPos == rowLgth - 1 && colPos > 7) break;
+
+			addr += i2c_row[i];
+			addr += i2c_col[j];
+
+			if (hexAddr.compare(addr) == 0) return true;
+		}
+	}
+
+	/*for (int i = 0; i < i2c_row.size(); i++)
 	{
 		for (int j = 0; j < i2c_col.size(); j++)
 		{
@@ -22,7 +42,7 @@ bool rpicomponents::pin::AddressChecker::IsI2CAddress(int address) {
 
 			if (hexAddr.compare(addr) == 0) return true;
 		}
-	}
+	}*/
 
 	return false;
 }
