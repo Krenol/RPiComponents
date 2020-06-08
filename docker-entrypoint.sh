30b2291dd7177b6b884666bf6f0564aa3385dfb5
krenol <lauritzgroeger@gmail.com>
2020-06-08 13:56:35 +0200

    improved docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,24 +2,11 @@
 set -e
 
 #set vars
-BUILD_DIR=/data/build
 EXEC=/data/rpicomponents/build/rpicomponents_test_project
 
-# SET BUILD_TEST IF IT WASN'T SET
-if [ "$BUILD_TEST" != 'ON' ] && [ "$BUILD_TEST" != 'OFF' ]; then
-    BUILD_TEST=OFF
-fi
-
-echo "BUILD_TEST=$BUILD_TEST"
 # RUN INPUT
-if [ "$1" = 'start' ] || [ "$1" = 'build' ]; then
-    cmake -D BUILD_TEST=$BUILD_TEST --build $BUILD_DIR
-    cmake --build $BUILD_DIR
-    if [ "$1" = 'start' ]; then
-        $EXEC
-    fi
+if [ "$1" = 'start' ]; then
+    $EXEC
 else 
     exec "$@"
 fi
-
-
